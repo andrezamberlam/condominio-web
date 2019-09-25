b03e7080c69a999bb883b9cb17259886649bcb4f
andrezamberlam - note <andrezamberlam@hotmail.com>
2019-09-25 01:14:32 -0300

    cadastro,exclusão de apartamentos

--- src/pages/Condominio/index.js
+++ src/pages/Condominio/index.js
@@ -3,11 +3,13 @@ import { Link, withRouter } from "react-router-dom";
 import api from "../../services/api";
 
 import './styles.css'
+import { async } from 'q';
+// import MultiSelect from 'multiselect-dropdown-react';
 
 const Apartamentos = ({ apartamentos }) => {
-    console.log(apartamentos);
+    // console.log(apartamentos);
     return (
-        <table>
+        <table >
             <thead>
                 <tr>
                     <th>Numero</th>
@@ -18,13 +20,12 @@ const Apartamentos = ({ apartamentos }) => {
             <tbody>
                 {apartamentos.length > 0 ? (
                     apartamentos.map(ap => (
-                        <tr key={ap.apartamentoid}>{console.log(ap.idapartamento)}
+                        <tr key={ap.idapartamento}>
                             <td>{ap.numero}</td>
                             <td>{ap.bloco}</td>
-                            {/* <td>{ap.username}</td> */}
                             <td>
                                 <button className="button muted-button">Edit</button>
-                                <button className="button muted-button">Delete</button>
+                                <button onClick={() => handleDelete(ap.idapartamento)} className="button muted-button">Delete</button>
                             </td>
                         </tr>
                     ))
@@ -38,29 +39,61 @@ const Apartamentos = ({ apartamentos }) => {
     )
 };
 
+const handleDelete = async(idapartamento) => {
+    const formData = new FormData();
+    formData.append('Idapartamento', idapartamento);
+
+    // new FormData();
+    var res = await api.post("/apartamento/excluir", formData);
+    
+    alert('Apartamento excluido');
+    window.location.href = '/app';
+
+}
 
 
 class Condominio extends Component {
     state = {
         apartamentos: [],
+        pessoas: [],
         numeroadd: null,
-        blocoadd:""
+        blocoadd: ""
 
 
         // password: "",
         // error: ""
     };
 
+
+
     handleAdd = async e => {
         e.preventDefault();
-        const { numeroadd, blocoadd } = this.state;
-        if (!numeroadd || blocoadd.length === 0) {
+
+        const { numeroadd, blocoadd, pessoas } = this.state;
+        // console.log(pessoas.map((p) => return));
+
+
+        let pessoasAP = pessoas.filter(function (elem, index, array) {
+            return elem.checked;
+        });
+
+        console.log(pessoasAP);
+
+        pessoasAP = pessoasAP.map((p) => {
+            return p.idpessoa
+        });
+
+        console.log(pessoasAP);
+
+        if (!numeroadd || blocoadd.length === 0 || pessoasAP.length === 0) {
             this.setState({ error: "Preencha todos os dados para se cadastrar" });
         } else {
             try {
                 const formData = new FormData();
                 formData.append('Numero', numeroadd);
                 formData.append('Bloco', blocoadd);
+                formData.append('Apartamentopessoa', JSON.stringify(pessoasAP));
+
                 // new FormData();
                 var res = await api.post("/apartamento/cadastrar", formData);
                 console.log(res);
@@ -80,18 +113,37 @@ class Condominio extends Component {
     //if (response.data.authenticated) {
     componentDidMount() {
         api.get('/apartamento').then(res => this.setState({ apartamentos: res.data }));
+        api.get('/pessoa').then(res => { this.setState({ pessoas: res.data }); });
+
         //this.setState();
         //alert('mahh oe');
+    }
+
+    handleChange(event, thiss) {
+        console.log('event.target.name', event.target.name);
+        console.log('event.target.value', event.target.value);
+        console.log('event.target.checked', event.target.checked);
+
+        //console.log(this);
+
+
+        this.setState({ pessoas: this.state.pessoas.map((p) => event.target.value == p.idpessoa ? { ...p, checked: event.target.checked } : { ...p }) });
+
+        console.log(this.state.pessoas);
+
+
     }
 
     render() {
         return (
             <React.Fragment>
                 <div className="container">
-                    <h1>CRUD App with Hooks</h1>
+                <button onClick={() => window.location.href = '/app' } >Apartamentos</button><hr />
+                <button onClick={() => window.location.href = '/pessoa' } >Pessoas</button>
+                    <h1>Apartamentos</h1>
                     <div className="flex-row">
                         <div className="flex-large">
-                            <h2>Add user</h2>
+                            <h2></h2>
                             {this.state.error && <p>{this.state.error}</p>}
                             <input
                                 type="number"
@@ -103,11 +155,25 @@ class Condominio extends Component {
                                 placeholder="Bloco"
                                 onChange={e => this.setState({ blocoadd: e.target.value })}
                             />
-                            <button onClick={this.handleAdd} >Cadastrar grátis</button>
+                            <div className="tabelaOverflow">
+                                {
+                                    this.state.pessoas.map(pes => (
+                                        <tr key={pes.idpessoa} >
+                                            <td>
+                                                <input type="checkbox" value={pes.idpessoa} onChange={e => this.handleChange(e, this)}></input>
+                                            </td>
+                                            <td>{pes.nome}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </div>
+                            <button onClick={this.handleAdd} >Cadastrar Apartamento</button>
                         </div>
                         <div className="flex-large">
                             <h2>Apartamentos</h2>
-                            <Apartamentos apartamentos={this.state.apartamentos} />
+                            <div className="tabelaOverflow">
+                                <Apartamentos apartamentos={this.state.apartamentos} />
+                            </div>
                             {/* <UserTable users={users} /> */}
                         </div>
                     </div>
